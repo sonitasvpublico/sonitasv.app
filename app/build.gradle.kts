d70d4f6ae8d0e29aada248750ede4366281692bb
Sonia Zavaleta Toukkari <soniazavaletatoukkari@Sonias-MacBook-Air.local>
2025-07-10 14:36:12 +0300

    Incremento de versionCode a 2 y versionName a 1.1 para nuevo release en Google Play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sonitasv.sonitasvapp"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
